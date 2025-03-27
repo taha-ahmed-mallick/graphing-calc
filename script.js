84821604fb131c12247ebb34da99a37fae510a44
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-03-28 03:28:51 +0500

    Basic translation complete 🎉ð!

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function resizeCanvas() {
 
 class Graph {
 	constructor(width, height) {
+		this.drag = false;
 		this.resizing(width, height);
 	}
 
@@ -26,24 +27,34 @@ class Graph {
 		ctx.translate(this.center[0], this.center[1]);
 		ctx.clearRect(-this.center[0], -this.center[1], this.width, this.height);
 		ctx.fillStyle = "#797979";
-		for (let i = -10; i < 11; i++) {
-			ctx.fillRect(this.unit[0] * i - 0.5, -this.height / 2, 1, this.height);
+		for (
+			let i = Math.floor(-this.center[0] / this.unit[0]);
+			i <= Math.floor((this.width - this.center[0]) / this.unit[0]);
+			i++
+		) {
+			ctx.fillRect(this.unit[0] * i - 0.5, -this.center[1], 1, this.height);
 		}
-		for (let i = -10; i < 11; i++) {
-			ctx.fillRect(-this.width / 2, this.unit[1] * i - 0.5, this.width, 1);
+		for (
+			let i = Math.floor(-this.center[1] / this.unit[1]);
+			i <= Math.floor((this.width - this.center[1]) / this.unit[1]);
+			i++
+		) {
+			ctx.fillRect(-this.center[0], this.unit[1] * i - 0.5, this.width, 1);
 		}
 		ctx.fillStyle = "#000";
-		/*x:*/ ctx.fillRect(-this.width / 2, -1, this.width, 2);
-		/*y:*/ ctx.fillRect(-1, -this.height / 2, 2, this.height);
+		/*x:*/ ctx.fillRect(-this.center[0], -1, this.width, 2);
+		/*y:*/ ctx.fillRect(-1, -this.center[1], 2, this.height);
 	}
 }
 
 let graph = new Graph(window.innerWidth, window.innerHeight);
 
 resizeCanvas();
-// cvs.addEventListener("mousedown", (eve) => console.log(eve));
-// cvs.addEventListener("mouseup", (eve) => console.log(eve));
+cvs.addEventListener("mousedown", () => (graph.drag = true));
+cvs.addEventListener("mouseup", () => (graph.drag = false));
+cvs.addEventListener("mouseleave", () => (graph.drag = false));
 cvs.addEventListener("mousemove", (eve) => {
+	if (!graph.drag) return null;
 	let x = eve.movementX,
 		y = eve.movementY;
 	console.log(graph.center);
