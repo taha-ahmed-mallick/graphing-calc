e93a9d45de087dd02e4ea0ff1ff2cc7d075aa45e
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-03-29 10:58:55 +0500

    graph tranlation process completed !

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function resizeCanvas() {
 class Graph {
 	constructor(width, height) {
 		this.drag = false;
+		this.touchCoord = [0, 0];
 		this.resizing(width, height);
 	}
 
@@ -27,23 +28,33 @@ class Graph {
 		ctx.translate(this.center[0], this.center[1]);
 		ctx.clearRect(-this.center[0], -this.center[1], this.width, this.height);
 		ctx.fillStyle = "#797979";
-		for (
-			let i = Math.floor(-this.center[0] / this.unit[0]);
-			i <= Math.floor((this.width - this.center[0]) / this.unit[0]);
-			i++
-		) {
+
+		// || to y-axis*
+		let yLimit = [
+			Math.floor(-this.center[0] / this.unit[0]),
+			Math.floor((this.width - this.center[0]) / this.unit[0]),
+		];
+		for (let i = yLimit[0]; i <= yLimit[1]; i++) {
 			ctx.fillRect(this.unit[0] * i - 0.5, -this.center[1], 1, this.height);
 		}
-		for (
-			let i = Math.floor(-this.center[1] / this.unit[1]);
-			i <= Math.floor((this.width - this.center[1]) / this.unit[1]);
-			i++
-		) {
+		// || to x-axis
+		let xLimit = [
+			Math.floor(-this.center[1] / this.unit[1]),
+			Math.floor((this.height - this.center[1]) / this.unit[1]),
+		];
+		for (let i = xLimit[0]; i <= xLimit[1]; i++) {
 			ctx.fillRect(-this.center[0], this.unit[1] * i - 0.5, this.width, 1);
 		}
 		ctx.fillStyle = "#000";
 		/*x:*/ ctx.fillRect(-this.center[0], -1, this.width, 2);
 		/*y:*/ ctx.fillRect(-1, -this.center[1], 2, this.height);
+
+		// point to draw on (m,n)
+		ctx.fillStyle = "#03A9F4";
+		let [m, n] = [0.5, -1];
+		ctx.arc(this.unit[0] * m, -this.unit[1] * n, 4, 0, Math.PI * 2);
+		ctx.fill();
+		ctx.beginPath();
 	}
 }
 
@@ -57,10 +68,23 @@ cvs.addEventListener("mousemove", (eve) => {
 	if (!graph.drag) return null;
 	let x = eve.movementX,
 		y = eve.movementY;
-	console.log(graph.center);
 	ctx.translate(-graph.center[0], -graph.center[1]);
 	graph.center[0] += x;
 	graph.center[1] += y;
 	graph.drawAxes();
-	console.log(graph.center);
+});
+
+cvs.addEventListener("touchstart", (eve) => {
+	graph.drag = true;
+	graph.touchCoord = [eve.changedTouches[0].pageX, eve.changedTouches[0].pageY];
+});
+cvs.addEventListener("touchend", () => (graph.drag = false));
+cvs.addEventListener("touchcancel", () => (graph.drag = false));
+cvs.addEventListener("touchmove", (eve) => {
+	let change = [eve.changedTouches[0].pageX - graph.touchCoord[0], eve.changedTouches[0].pageY - graph.touchCoord[1]];
+	graph.touchCoord = [eve.changedTouches[0].pageX, eve.changedTouches[0].pageY];
+	ctx.translate(-graph.center[0], -graph.center[1]);
+	graph.center[0] += change[0];
+	graph.center[1] += change[1];
+	graph.drawAxes();
 });
